41d765cad3d11aa321a7730c489c991452134e95
Heavenjes <paulinecalatrava@gmail.com>
2019-11-12 13:24:26 +0800

    hasula aneh oie

--- src/modules/Home.vue
+++ src/modules/Home.vue
@@ -8,11 +8,10 @@
       </div>
       <div class="col-sm">
         <img id="image1" src="@/assets/image4.jpg" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <img id="image2" src="@/assets/image2.jpg" />
-        <!-- <img id="image" src="@/assets/image3.jpg" />
-          <img id="image" src="@/assets/image4.jpg" />
-          <img id="image" src="@/assets/image5.jpg" />
-        <img id="image" src="@/assets/image6.jpg" />-->
+        <img id="image2" src="@/assets/image5.jpg" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <img id="image3" src="@/assets/image2.jpg" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <img id="image4" src="@/assets/image3.jpg" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          
       </div>
     </div>
   </div>
@@ -30,16 +29,36 @@
   margin-top: 90px !important;
 }
 #image1 {
-  width: 400px !important;
+  width: 450px !important;
+   margin-top: 0% !important;
   -ms-transform: rotate(20deg); /* IE 9 */
   -webkit-transform: rotate(20deg); /* Safari 3-8 */
   transform: rotate(20deg);
 }
 #image2 {
-  width: 250px !important;
-    margin-top: 100px !important;
+  width: 330px !important;
+  margin-top: 6% !important;
+  margin-left:5px;
   -ms-transform: rotate(20deg); /* IE 9 */
   -webkit-transform: rotate(20deg); /* Safari 3-8 */
+  transform: rotate(20deg);
+}
+
+#image3 {
+  width: 300px !important;
+  margin-top: 0% !important;
+  margin-left:290px;
+ -ms-transform: rotate(20deg); /* IE 9 */
+ -webkit-transform: rotate(20deg); /* Safari 3-8 */
+  transform: rotate(20deg);
+}
+
+#image4{
+  width: 340px !important;
+  margin-top: 0% !important;
+  margin-left:161px;
+ -ms-transform: rotate(20deg); /* IE 9 */
+ -webkit-transform: rotate(20deg); /* Safari 3-8 */
   transform: rotate(20deg);
 }
 </style>
